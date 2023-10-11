8df788a88526f94af576fe91cd74c80b1b237728
Ralf1986 <ralfsantos.oliveira@gmail.com>
2023-10-10 22:13:26 -0300

    Initial commit

---
+++ src/br/com/cod3r/calc/visao/Botao.java
@@ -0,0 +1,24 @@
+package br.com.cod3r.calc.visao;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Botao extends JButton {
+
+    public Botao(String texto, Color cor){
+        setText(texto);
+        setFont(new Font("courier", Font.PLAIN, 30));
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+
+
+
+    }
+
+
+
+
+}
